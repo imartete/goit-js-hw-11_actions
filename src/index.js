0388ceec43d1111d9149f2fc7238d6dc854da2f5
Marta <maardiakiv@gmail.com>
2022-11-16 12:29:41 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,73 +7,57 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const inputFormNode = document.querySelector('.search-form');
 const galleryNode = document.querySelector('.gallery');
 const loadBtnNode = document.querySelector('.load-more');
-let searchQuery, lightbox, currentPage, totalPages;
+let searchQuery, currentPage, totalPages, totalHits;
 
 inputFormNode.addEventListener('submit', event => {
   event.preventDefault();
-
   searchQuery = event.target.elements.searchQuery.value.trim();
   currentPage = 1;
-
-  fetchImages(searchQuery, currentPage)
-    .then(data => {
-      const imagesArray = data.hits;
-      totalPages = Math.ceil(data.totalHits / 40);
-
-      if (!imagesArray.length) {
-        Notify.failure(
-          '"Sorry, there are no images matching your search query. Please try again."'
-        );
-        galleryNode.textContent = '';
-        loadBtnNode.classList.add('hidden');
-      } else {
-        Notify.success(`"Hooray! We found ${data.totalHits} images."`);
-        galleryNode.innerHTML = makeGallery(imagesArray).join('');
-        loadBtnNode.classList.remove('hidden');
-        inputFormNode.reset();
-      }
-      if (imagesArray.length && currentPage >= totalPages) {
-        Notify.info(
-          '"We\'re sorry, but you\'ve reached the end of search results."'
-        );
-        loadBtnNode.classList.add('hidden');
-      }
-      lightbox = new SimpleLightbox('.gallery a');
-    })
-    .catch(error => console.log(error));
+  galleryNode.textContent = '';
+  loadGallery();
 });
 
 loadBtnNode.addEventListener('click', event => {
   event.preventDefault();
+  currentPage += 1;
+  loadGallery();
+});
 
-  if (event.target.click) {
-    currentPage += 1;
-  } else {
-    currentPage = 1;
-  }
-
+function loadGallery() {
   fetchImages(searchQuery, currentPage)
     .then(data => {
-      galleryNode.insertAdjacentHTML(
-        'beforeend',
-        makeGallery(data.hits).join('')
-      );
-      lightbox.refresh();
-      if (currentPage >= totalPages) {
-        Notify.info(
-          '"We\'re sorry, but you\'ve reached the end of search results."'
+      const imagesArray = data.hits;
+      totalPages = Math.ceil(data.totalHits / 40);
+      totalHits = data.totalHits;
+
+      if (!imagesArray.length) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
         );
-        loadBtnNode.classList.add('hidden');
+      } else {
+        galleryNode.insertAdjacentHTML(
+          'beforeend',
+          makeGallery(data.hits).join('')
+        );
+        loadBtnNode.classList.remove('hidden');
+        inputFormNode.reset();
+        new SimpleLightbox('.gallery a').refresh();
+        if (currentPage === totalPages) {
+          Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+          loadBtnNode.classList.add('hidden');
+        }
       }
     })
-    .catch(error => console.log(error));
-});
+    .catch(error => console.log(error))
+    .finally(loadBtnNode.classList.add('hidden'));
+}
 
 function makeGallery(array) {
   return array.map(el => {
     return `
-    <a href="${el.largeImageURL}">
-    <div class="photo-card">
+    <a href="${el.largeImageURL}" class="photo-card">
     <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
     <div class="info">
       <p class="info-item">
@@ -89,7 +73,6 @@ function makeGallery(array) {
         <b>Downloads ${el.downloads}</b>
       </p>
     </div>
-  </div>
   </a>`;
   });
 }
